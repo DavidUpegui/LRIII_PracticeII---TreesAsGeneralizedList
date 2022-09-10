f9dcebbc58a14f6e29405af4e847184bdbb78dc5
DavidUpegui <david.upegui1@udea.edu.co>
2022-09-10 00:50:55 -0500

    Methods completed: gradoElemento, grado, elementoEsPadre, buscarDato, ancestros, muestraArbol -- Methods fixed: construyeArbol

--- js/ArbolLG.js
+++ js/ArbolLG.js
@@ -27,15 +27,14 @@ class ArbolLG{
         let pila = new Pila();
         this.string = newS;
         this.raiz = new NodoLg(null);
-        console.log(`El dato que entra en la raíz es ${newS[1]}`);
         this.raiz.asignaDato(newS[1]);
-        if(s.length === 2){
+        if(newS.length === 2){
             return
         }
         let x = new NodoLg(null);
         this.raiz.asignaLiga(x);
         let ultimo = x;
-        for(let i = 3; i <= s.length - 2; i++){
+        for(let i = 3; i <= newS.length - 2; i++){
             switch(newS[i]){
                 case '(':
                     contParenthesis++
@@ -57,21 +56,174 @@ class ArbolLG{
                     contParenthesis--
                     break
                 default:
-                    ultimo.asignaDato(s[i]);
+                    ultimo.asignaDato(newS[i]);
             }
         }
     }
 
-    altura(){
-
+    muestraArbol(){
+        let pila = [];
+        let p = this.raiz;
+        while(pila.length !== 0 || p != null){
+            if(p === null){
+                p = pila.pop()
+            }else{
+                if(p.retornaSw() === 1){
+                    pila.push(p);
+                    p = p.retornaDato()
+                }
+                console.log(p.retornaDato())
+            }
+            p = p.retornaLiga();
+        }
     }
+
+    ancestros(e){
+        let pilaRecorrido = [];
+        let pilaAncestros = [];
+        let p = this.raiz;
+        pilaAncestros.push(p.retornaDato());
+        while(pilaRecorrido.length !== 0 || p !== null){
+            if(p === null){
+                p = pilaRecorrido.pop();
+                pilaAncestros.pop();
+            }else{
+                if(p.retornaSw() === 1){
+                    pilaRecorrido.push(p);
+                    p = p.retornaDato();
+                    if(p.retornaDato() === e) break
+                    else{
+                        pilaAncestros.push(p.retornaDato());
+                    }
+                }else{
+                    if(p.retornaDato() === e) break
+                }
+            }
+            p = p.retornaLiga();
+        }
+        let str = ''
+        while(pilaAncestros.length !== 0){
+            if(pilaAncestros.length === 1){
+                str = `${str}${pilaAncestros.pop()}`;
+            }else{
+                str = `${str}${pilaAncestros.pop()}, `;
+            }
+        }
+        return str
+    }
+
+    buscarDato(d){
+        let pila = [];
+        let p = this.raiz;
+        while(pila.length !== 0 || p != null){
+            if(p === null){
+                p = pila.pop()
+            }else{
+                if(p.retornaSw() === 1){
+                    pila.push(p);
+                    p = p.retornaDato()
+                }
+                if(p.retornaDato() === d){
+                    return p
+                }
+            }
+            p = p.retornaLiga();
+        }
+        return 'Dato no encontrado';
+    }
+
+    elementoEsPadre(d){
+        let pila = [];
+        let p = this.#raiz;
+        if(d === p.retornaDato()) return true;
+        let bandera = false;
+        while(pila.length !== 0 || p !== null){
+            bandera = false;
+            if(p === null){
+                p = pila.pop()
+            }else{
+                if(p.retornaSw() === 1){
+                    pila.push(p);
+                    p = p.retornaDato()
+                    bandera = true;
+                }
+                if(p.retornaDato() === d){
+                    return bandera;
+                }
+            }
+            p = p.retornaLiga();
+        }
+    }
+
     grado(){
-
+        let q;
+        let pila = [];
+        let p = this.#raiz;
+        let grado = 0;
+        q = p.retornaLiga();
+        while(q !== null){
+            grado++;
+            q = q.retornaLiga();
+        }
+        let maxGrado = grado;
+        while(pila.length !== 0 || p != null){
+            grado = 0;
+            if(p === null){
+                p = pila.pop()
+            }else{
+                if(p.retornaSw() === 1){
+                    pila.push(p);
+                    p = p.retornaDato();
+                    q = p.retornaLiga()
+                    while(q !== null){
+                        grado++;
+                        q = q.retornaLiga();
+                    }
+                    if(grado > maxGrado){
+                        maxGrado = grado;
+                    }
+                }
+            }
+            p = p.retornaLiga();
+        }
+        return maxGrado
     }
 
-    containElement(el){
-        return this.string.includes(el);
+    gradoElemento(el){
+        let pila = []
+        let p = this.raiz;
+        let grado = 0;
+        if(el === p.retornaDato()){
+            while(p !== null){
+                grado++
+                p = p.retornaLiga()
+            }
+            return grado - 1;
+        }
+        while(pila.length !== 0 || p !== null){
+            if(p===null){
+                p = pila.pop();
+            }else{
+                if(p.retornaSw()=== 1){
+                    pila.push(p);
+                    p = p.retornaDato()
+                    if(p.retornaDato() === el){
+                        p = p.retornaLiga();
+                        while(p !== null){
+                            grado++
+                            p = p.retornaLiga()
+                        }
+                        return grado;
+                    }
+                }
+                if(p.retornaDato() === el){
+                    return grado;
+                }
+            }
+            p = p.retornaLiga()
+        }
     }
 }
+
 
 
